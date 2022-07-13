ee77b0193e1cd69c35753485ecd9e483d6522e12
Narekmaralchyan <narekmaralchyan@gmail.com>
2022-07-13 21:03:57 +0400

    first commit

---
+++ task10.js
@@ -0,0 +1,12 @@
+// 10)
+// 1. Make a new folder called 'Workshop_4'
+//
+// 2. make that folder into git repo
+//
+// 2. create a branch called rename_me
+//
+// 3. create a branch called delete_me
+//
+// 4. rename the branch rename_me to renamed_branch
+//
+// 5. delete the branch delete_me
